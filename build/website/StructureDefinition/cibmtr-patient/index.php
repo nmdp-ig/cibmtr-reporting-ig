0b23fdff51381010b4cbe2b63abef420e5b05811
bmilius-nmdp <bmilius@nmdp.org>
2021-03-04 11:05:57 -0600

    added valuesets

--- build/website/StructureDefinition/cibmtr-patient/index.php
+++ build/website/StructureDefinition/cibmtr-patient/index.php
@@ -7,19 +7,19 @@ function Redirect($url)
 
 $accept = $_SERVER['HTTP_ACCEPT'];
 if (strpos($accept, 'application/json+fhir') !== false)
-  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/2020-11/StructureDefinition-cibmtr-patient.json2');
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/2021-03/StructureDefinition-cibmtr-patient.json2');
 elseif (strpos($accept, 'application/fhir+json') !== false)
-  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/2020-11/StructureDefinition-cibmtr-patient.json1');
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/2021-03/StructureDefinition-cibmtr-patient.json1');
 elseif (strpos($accept, 'json') !== false)
-  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/2020-11/StructureDefinition-cibmtr-patient.json');
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/2021-03/StructureDefinition-cibmtr-patient.json');
 elseif (strpos($accept, 'application/xml+fhir') !== false)
-  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/2020-11/StructureDefinition-cibmtr-patient.xml2');
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/2021-03/StructureDefinition-cibmtr-patient.xml2');
 elseif (strpos($accept, 'application/fhir+xml') !== false)
-  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/2020-11/StructureDefinition-cibmtr-patient.xml1');
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/2021-03/StructureDefinition-cibmtr-patient.xml1');
 elseif (strpos($accept, 'html') !== false)
-  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/2020-11/StructureDefinition-cibmtr-patient.html');
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/2021-03/StructureDefinition-cibmtr-patient.html');
 else 
-  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/2020-11/StructureDefinition-cibmtr-patient.xml');
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/2021-03/StructureDefinition-cibmtr-patient.xml');
 ?>
     
 You should not be seeing this page. If you do, PHP has failed badly.

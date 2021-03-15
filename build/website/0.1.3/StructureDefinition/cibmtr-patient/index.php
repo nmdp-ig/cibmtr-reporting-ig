bebd43d2a285cbeb1600e5c4171bfe8ddecf21ef
bmilius-nmdp <bmilius@nmdp.org>
2021-03-15 17:44:08 -0500

    added website to git

---
+++ build/website/0.1.3/StructureDefinition/cibmtr-patient/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/0.1.3/StructureDefinition-cibmtr-patient.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/0.1.3/StructureDefinition-cibmtr-patient.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/0.1.3/StructureDefinition-cibmtr-patient.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/0.1.3/StructureDefinition-cibmtr-patient.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/0.1.3/StructureDefinition-cibmtr-patient.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/0.1.3/StructureDefinition-cibmtr-patient.html');
+else 
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/0.1.3/StructureDefinition-cibmtr-patient.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
---
+++ build/website/0.1.3/ValueSet/gvhdtype-valueset/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/0.1.3/ValueSet-gvhdtype-valueset.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/0.1.3/ValueSet-gvhdtype-valueset.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/0.1.3/ValueSet-gvhdtype-valueset.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/0.1.3/ValueSet-gvhdtype-valueset.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/0.1.3/ValueSet-gvhdtype-valueset.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/0.1.3/ValueSet-gvhdtype-valueset.html');
+else 
+  Redirect('http://fhir.nmdp.org/ig/cibmtr-reporting/0.1.3/ValueSet-gvhdtype-valueset.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
